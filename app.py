7b3084e1a4386c86ed553fccf23a72e484db6d84
kimjoshjp <kimjoshjp@gmail.com>
2019-10-30 16:41:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 #
 #
 # If application.py
-gunicorn --bind=0.0.0.0 --timeout 600 application:app
+#gunicorn --bind=0.0.0.0 --timeout 600 application:app
   
 # If app.py
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
