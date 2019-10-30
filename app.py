2bc4ed345b6f7fc142e9cf7a6f2e6a7606c33bb9
kimjoshjp <kimjoshjp@gmail.com>
2019-10-30 16:13:19 +1000

    Create app.py
    For azure env

---
+++ app.py
@@ -0,0 +1,11 @@
+# In Azure env, in oder to running flask
+# 
+#
+#
+#
+
+# If application.py
+#gunicorn --bind=0.0.0.0 --timeout 600 application:app
+
+# If app.py
+gunicorn --bind=0.0.0.0 --timeout 600 app:app
